f342cc8a6cd7684fa11bb24ca6a31a98ab14ce31
basha-masthan <bashamasthan31@gmail.com>
2024-05-16 12:54:12 +0530

    day2

---
+++ Day-2/2D arrays/info_2D_Array.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+
+int k[10][10];
+
+void trace(int,int);
+void diagonal(int,int);
+
+void main(){
+    int n,m,i,j;
+    printf("Enter no of Rows & columns:");
+    scanf("%d %d",&n,&m);
+    int a[n][m],sum=0;
+
+    // Matrix Input
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            printf("a[%d][%d]:",i,j);
+            scanf("%d",&a[i][j]);
+            k[i][j]=a[i][j];
+        }
+    }
+
+    // Matrix Printing
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            printf(" %d ",a[i][j]);
+        }
+         printf("\n");
+    }
+
+    trace(n,m);
+    diagonal(n,m);
+
+}
+
+
+void trace(int n,int m){
+    int i,j,sum=0;
+     for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            if(i==j || i+j==n-1)
+                sum = sum + k[i][j];
+        }
+    }
+     printf("Trace of a Matrix : %d\n",sum);
+}
+
+void diagonal(int n,int m){
+     int i,j,sum=0;
+     for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            if(i==j || i+j==n-1)
+                continue;
+            else
+                sum = sum + k[i][j];
+        }
+    }
+     printf("After Trace of a Matrix : %d\n",sum);
+}
+
+void up_triangle(int n,int m){
+     int i,j,sum=0,k;
+     for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            if (i>j)
+                k[i][j]=0;
+        }
+    }
+     for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            printf(" %d ",k[i][j]);
+        }
+         printf("\n");
+    }
+}
